ae723cae680b2cac63f311704549fb50023f1c4d
moloko <mleathes+github@gmail.com>
2020-11-04 11:42:16 +0000

    sort out bad indentation

--- js/adapt-scrollPrompt.js
+++ js/adapt-scrollPrompt.js
@@ -1,25 +1,24 @@
 define([
-  'core/js/adapt',
-  './scrollPromptView'
+	'core/js/adapt',
+	'./scrollPromptView'
 ], function(Adapt, ScrollPromptView) {
 
-		Adapt.on('menuView:ready pageView:ready', function(view) {
+	Adapt.on('menuView:ready pageView:ready', function(view) {
+		var model = view.model;
 
-			var model = view.model;
+		var scrollPrompt = model.get('_scrollPrompt');
+		if (!scrollPrompt || !scrollPrompt._isEnabled) return;
 
-			var scrollPrompt = model.get('_scrollPrompt');
-			if (!scrollPrompt || !scrollPrompt._isEnabled) return;
+		var modelType = model.get('_type');
+		if (modelType === 'course') modelType = 'menu';
 
-    var modelType = model.get('_type');
-    if (modelType === 'course') modelType = 'menu';
+		/* set model type selector to append scroll prompt to */
+		var modelTypeSelector = '.' + modelType + '__header-inner';
 
-    /* set model type selector to append scroll prompt to */
-			var modelTypeSelector = '.' + modelType + '__header-inner';
+		new ScrollPromptView({
+			model: model
+		}).$el.appendTo(view.$(modelTypeSelector));
 
-    new ScrollPromptView({
-      model: model
-			}).$el.appendTo(view.$(modelTypeSelector));
-
-		});
+	});
 
 });
