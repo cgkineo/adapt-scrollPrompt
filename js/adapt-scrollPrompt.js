b7c5fd4fd88050cc15471d41aad357283924f4e2
Matt Leathes <matt.leathes@kineo.com>
2020-11-06 16:09:57 +0000

    tabs > spaces

--- js/adapt-scrollPrompt.js
+++ js/adapt-scrollPrompt.js
@@ -1,24 +1,25 @@
 define([
-	'core/js/adapt',
-	'./scrollPromptView'
+  'core/js/adapt',
+  './scrollPromptView'
 ], function(Adapt, ScrollPromptView) {
 
-	Adapt.on('menuView:ready pageView:ready', function(view) {
-		var model = view.model;
+  Adapt.on('menuView:ready pageView:ready', function(view) {
 
-		var scrollPrompt = model.get('_scrollPrompt');
-		if (!scrollPrompt || !scrollPrompt._isEnabled) return;
+    var model = view.model;
 
-		var modelType = model.get('_type');
-		if (modelType === 'course') modelType = 'menu';
+    var scrollPrompt = model.get('_scrollPrompt');
+    if (!scrollPrompt || !scrollPrompt._isEnabled) return;
 
-		/* set model type selector to append scroll prompt to */
-		var modelTypeSelector = '.' + modelType + '__header-inner';
+    var modelType = model.get('_type');
+    if (modelType === 'course') modelType = 'menu';
 
-		new ScrollPromptView({
-			model: model
-		}).$el.appendTo(view.$(modelTypeSelector));
+    /* set model type selector to append scroll prompt to */
+    var modelTypeSelector = '.' + modelType + '__header-inner';
 
-	});
+    new ScrollPromptView({
+      model: model
+    }).$el.appendTo(view.$(modelTypeSelector));
+
+  });
 
 });
