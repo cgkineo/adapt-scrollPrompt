a47a2a3d276f418693e389b08dc643c90077ff2f
Brad Simpson <brad.simpson@kineo.com>
2020-11-18 12:29:25 -0700

    Add ability to apply scroll prompt to components

--- js/adapt-scrollPrompt.js
+++ js/adapt-scrollPrompt.js
@@ -3,15 +3,26 @@ define([
   './scrollPromptView'
 ], function(Adapt, ScrollPromptView) {
 
-  Adapt.on('menuView:ready pageView:ready', function(view) {
+  Adapt.on('menuView:ready pageView:ready componentView:postRender', function(view) {
 
     var model = view.model;
 
     var scrollPrompt = model.get('_scrollPrompt');
     if (!scrollPrompt || !scrollPrompt._isEnabled) return;
 
-    var modelType = model.get('_type');
-    if (modelType === 'course') modelType = 'menu';
+    var modelType;
+
+    switch (model.get("_type")) {
+      case "page":
+        modelType = "page";
+        break;
+      case "course":
+        modelType = "menu";
+        break;
+      case "component":
+        modelType = "component";
+        break;
+    }
 
     /* set model type selector to append scroll prompt to */
     var modelTypeSelector = '.' + modelType + '__header-inner';
